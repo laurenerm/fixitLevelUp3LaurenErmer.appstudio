da7cfda1fa7b1282564711e94cfca304361e3ac9
Lauren <laurenermer@hotmail.com>
2019-09-26 22:18:35 -0500

    some updates :)

---
+++ forms/mainForm/mainForm.js
@@ -0,0 +1,15 @@
+
+
+alert('Let\'s calculate your loan payments! Are you ready?');
+let x = prompt('What is the amount of your loan?');
+let yOne = prompt('What is the interest rate on your loan?');
+let z = prompt('How many months is this loan for?');
+let y = Number(yOne)/12;
+let choice = prompt('Which would you like to calculate? Input car for car loan payments or home for home loan payments');
+if (choice= 'car') {
+console.log(`A car loan for ${x} over ${z} months at a rate of ${yOne} is ${carLoanPayment(Number(x), Number(y), Number(z))}.`);
+}
+else {
+console.log('no');
+}
+
